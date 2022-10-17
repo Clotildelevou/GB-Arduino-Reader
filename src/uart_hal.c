d084c6e21477115d1243205690dba3fcc0fd608b
Clotilde Levesque <clotilde.levesque@epita.fr>
2022-10-17 13:15:17 +0200

    uart: tx functions reordered

--- src/uart_hal.c
+++ src/uart_hal.c
@@ -2,41 +2,44 @@
 
 #define F_CPU 16000000
 
+volatile static uint8_t rx_buf[RX_BUF_SIZE] = {0};
+volatile static uint16_t rx_count = 0;
 
 
 void uart_init(uint32_t baudrate)
 {
-  baudrate = F_CPU / (16 * baudrate) - 1;
-  //Set baudrate
-  UBRR0H = (baudrate & 0x0F00) >> 8;
-  UBRR0L = baudrate & 0x00FF;
+    baudrate = F_CPU / (16 * baudrate) - 1;
+    //Set baudrate
+    UBRR0H = (baudrate & 0x0F00) >> 8;
+    UBRR0L = baudrate & 0x00FF;
 
-  //Enable TX and RX and interruption
-  UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << TXCIE0) | (1 << RXCIE0);
-
+    //Enable TX and RX and interruption
+    UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << TXCIE0) | (1 << RXCIE0);
+    sei();
 }
 
 void uart_send_byte(uint8_t c)
 {
-  while (!(UCSR0A & (1 << UDRE0)));
-  UDR0 = c;
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = c;
 }
 
 void uart_send_buffer(uint8_t *buf, uint16_t len)
 {
-  for(int i = 0; i < len; i++)
-  {
-    uart_send_byte(buf[i]);
-  }
+    for(int i = 0; i < len; i++)
+    {
+        uart_send_byte(buf[i]);
+    }
 }
 
 void uart_send_string(uint8_t *str)
 {
-  while (*str != '\0')
-  {
-    uart_send_byte(*str);
-    str++;
-  }
+    while (*str != '\0')
+    {
+        uart_send_byte(*str);
+        str++;
+    }
+}
 
 
 ISR(USART_TX_vect)
